73d049ed89a64cb564e0e30d6ea1f36ebc663ed9
oppofind <836575280@qq.com>
2017-12-24 16:02:38 +0800

    修改单元测试模板

--- springboot-mutiple-dbsource01/src/test/java/com/boco/sunyu/controller/ControllerBaseTest.java
+++ springboot-mutiple-dbsource01/src/test/java/com/boco/sunyu/controller/ControllerBaseTest.java
@@ -1,10 +1,15 @@
 package com.boco.sunyu.controller;
 
+import com.boco.sunyu.config.DataSourceConfig;
+import com.boco.sunyu.config.TransactionManagerConfig;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by ApplicationPower.
@@ -12,8 +17,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@EnableTransactionManagement(proxyTargetClass = true)
+@ContextConfiguration(classes = { DataSourceConfig.class, TransactionManagerConfig.class })
+@Transactional(value = "transactionManager", rollbackFor = Exception.class)
 @AutoConfigureMockMvc
-public class ControllerBaseTest {
+public class ControllerBaseTest extends AbstractTransactionalJUnit4SpringContextTests {
 
 }
