adb8c54640cd6fa662c52daaf2467b7b55e44007
oppofind <836575280@qq.com>
2018-03-09 23:48:52 +0800

    添加redis的mq实现

---
+++ SpringRedisMQ/src/main/java/com/sunyu/concurrent/redis/mq/SendMessage.java
@@ -0,0 +1,21 @@
+package com.sunyu.concurrent.redis.mq;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+/**
+ * 消息生产者
+ */
+@Component
+public class SendMessage {
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public void sendMessage(String channel, Serializable message) {
+        redisTemplate.convertAndSend(channel, message);
+    }
+
+}
