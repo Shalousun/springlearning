a9a28cc3e0988440cf8cb546f53ce3f2cc638172
oppofind <836575280@qq.com>
2018-11-12 20:53:57 +0800

    修改apollo测试demo

--- SpringBoot2-Apollo/src/main/java/com/sunyu/apollo/SpringBootMainApplication.java
+++ SpringBoot2-Apollo/src/main/java/com/sunyu/apollo/SpringBootMainApplication.java
@@ -1,16 +1,28 @@
 package com.sunyu.apollo;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import com.sunyu.apollo.util.TestJavaConfigBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author yu on 2018/11/12.
  */
 @SpringBootApplication
+@Configuration
+@EnableApolloConfig
 @MapperScan("com.sunyu.apollo.dao")
 public class SpringBootMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMainApplication.class, args);
+    }
+
+
+    @Bean
+    public TestJavaConfigBean javaConfigBean() {
+        return new TestJavaConfigBean();
     }
 }
