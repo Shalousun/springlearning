0d6be4607c4b6ae2a2cedf1e5d1fe2d051297236
oppofind <836575280@qq.com>
2019-03-16 23:15:37 +0800

    完善接口

--- SpringBoot2-Redis/src/main/java/com/sunyu/redis/cache/RedisCacheService.java
+++ SpringBoot2-Redis/src/main/java/com/sunyu/redis/cache/RedisCacheService.java
@@ -1,7 +1,9 @@
 package com.sunyu.redis.cache;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -19,7 +21,7 @@ public interface RedisCacheService {
     void set(String key, Serializable value);
 
     /**
-     * 存活时间分钟
+     * 默认是秒
      *
      * @param key
      * @param value
@@ -28,7 +30,7 @@ public interface RedisCacheService {
     void set(String key, Serializable value, Long timeOut);
 
     /**
-     * 设置缓存
+     * 设置key和value的超时时间(设置成String返回类型,不然要设置成Void)
      *
      * @param key
      * @param value
@@ -68,27 +70,94 @@ public interface RedisCacheService {
 
     /**
      * @param key
+     * @param <T>
      * @return
      */
-    boolean hasKey(String key);
+    <T> Set<T> sGetAll(final String key, Class<T> tClass);
 
     /**
      *
      * @param key
+     * @return
+     */
+    Set<String> sGetAll(final String key);
+    /**
+     * 检查是否是set集合中元素
+     * @param key Redis key
+     * @param member Set member
+     * @return
+     */
+    Boolean isMember(String key, Object member);
+
+    /**
+     * 返回元素
+     * @param key
+     * @param tClass
+     * @return
+     */
+    <V> List<V> sMembersList(String key, Class<V> tClass);
+
+    /**
+     * 从Set中删除元素
+     * @param key key
+     * @param member
+     * @return
+     */
+    Long sRemove(String key,Object member);
+
+    /**
+     * @param key
+     * @return
+     */
+    boolean hasKey(String key);
+    /**
+     * Atomically increments the current value by one.
+     *
+     * @param key
+     * @return
+     */
+    Long increment(String key);
+
+    /**
+     * Atomically increments the current value by delta.
+     * @param key
      * @param delta
      * @return
      */
-    long increment(String key, long delta);
+    Long increment(String key,long delta);
+
+    /**
+     * Set a timeout for object. After the timeout has expired,
+     * the key will automatically be deleted.
+     *
+     * @param key     key
+     * @param timeout timeout before object will be deleted
+     * @param unit    timeout time unit
+     * @return
+     */
+    Boolean expire(String key, final long timeout, final TimeUnit unit);
+
+    /**
+     * 让某个key在某一个时间点失效
+     *
+     * @param key
+     * @param unixTime
+     * @return
+     */
+    Boolean expireAt(final String key, final long unixTime);
 
 
     /**
      * @param key
+     * @param hKey
+     * @param hValue
+     */
+    void hSet(String key, String hKey, Object hValue);
+    /**
+     * @param key
      * @param map
      */
-    void hSet(String key, Map<String, String> map);
-
-    void hset(String key, Map<String, Object> map);
-
+    void hSet(String key, Map<String,Object> map);
 
     /**
      * 获取hash类型的集合
@@ -104,4 +173,131 @@ public interface RedisCacheService {
      * @return
      */
     Map<String,String> hGetAll(String key);
+
+    /**
+     * @param key
+     * @param hashKey
+     * @param tClass
+     * @return
+     */
+    <V> V hGet(String key, String hashKey,Class<V> tClass);
+
+    /**
+     * @param key
+     * @param hashKey
+     * @return
+     */
+    String hGet(String key, String hashKey);
+
+
+    /**
+     *
+     * @param key
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    <T> List<T> hGetAllList(String key,Class<T> clazz);
+
+    /**
+     * 指定 list 从左入栈
+     *
+     * @param key
+     * @param value
+     * @return length of current stack
+     */
+    Long leftPush(final String key, Object value);
+
+    /**
+     * 指定 list 从左入栈多个value
+     *
+     * @param key
+     * @param values
+     * @return length of current stack
+     */
+    Long leftPushAll(final String key, Object... values);
+
+
+    /**
+     * list 从左出栈
+     * 如果列表没有元素,会堵塞到列表一直有元素或者超时为止
+     *
+     * @param key
+     * @param <T>
+     * @return
+     */
+    <T> T leftPop(final String key);
+
+
+    /**
+     * 指定 list 从右入栈
+     *
+     * @param key
+     * @param value
+     * @return length of current stack
+     */
+    Long rightPush(final String key, Object value);
+
+    /**
+     * 指定 list 从右入栈多个value
+     *
+     * @param key
+     * @param values
+     * @return length of current stack
+     */
+    Long rightPushAll(final String key, Object... values);
+
+    /**
+     * list 从右出栈
+     * 移除并获取列表最后一个元素
+     *
+     * @param key
+     * @param <T>
+     * @return
+     */
+    <T> T rightPop(final String key);
+
+    /**
+     * 获取列表指定范围内的元素
+     *
+     * @param key
+     * @param start 开始位置, 0是开始位置
+     * @param end   结束位置, -1返回所有
+     * @return
+     */
+    <T> List<T> lRange(String key, long start, long end,Class<T> tClass);
+
+
+    /**
+     * 删除hash key
+     *
+     * @param key
+     * @param hashKey
+     */
+    long hDel(String key, String hashKey);
+
+    /**
+     * 查询key的剩余时间(time to live)
+     * @param key
+     * @return
+     */
+    long ttl(String key);
+    /**
+     * 检查hash key
+     * @param key key
+     * @param hashKey hashKey
+     * @return
+     */
+    boolean hasHKey(String key, String hashKey);
+
+
+    /**
+     * redis SETNX命令
+     * 已存在返回true, 否则返回false
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    boolean setNx(String key, Object value);
 }
--- SpringBoot2-Redis/src/main/java/com/sunyu/redis/cache/RedisCacheServiceImpl.java
+++ SpringBoot2-Redis/src/main/java/com/sunyu/redis/cache/RedisCacheServiceImpl.java
@@ -3,14 +3,18 @@ package com.sunyu.redis.cache;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * @author yu 2018/11/08.
@@ -19,36 +23,39 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class RedisCacheServiceImpl implements RedisCacheService {
 
+    private static final Logger logger = LoggerFactory.getLogger(RedisCacheServiceImpl.class);
     @Autowired
     private RedisTemplate redisTemplate;
 
     @Override
     public void set(String key, Serializable value) {
-        log.debug("redis set key: {}", key);
+        logger.debug("redis set key: {}", key);
         redisTemplate.opsForValue().set(key, value);
     }
 
     @Override
     public void set(String key, Serializable value, Long timeOut) {
-        log.debug("redis set key: {}", key);
-        this.set(key, value, timeOut, TimeUnit.MINUTES);
+        logger.debug("redis set key: {}", key);
+        this.set(key, value, timeOut, TimeUnit.SECONDS);
     }
 
     @Override
     public void set(String key, Serializable value, Long timeOut, TimeUnit timeUnit) {
-        log.debug("redis set key: {}", key);
-        redisTemplate.opsForValue().set(key, value, timeOut, timeUnit);
+        logger.debug("setEX redis key: {} value: {} timeOut: {}", key, JSON.toJSONString(value), timeOut, timeUnit);
+        if (StringUtils.isNotEmpty(key)) {
+            redisTemplate.opsForValue().set(key, value, timeOut, timeUnit);
+        }
     }
 
     @Override
     public void delete(String key) {
-        log.debug("redis delete key: {}", key);
+        logger.debug("redis delete key: {}", key);
         redisTemplate.delete(key);
     }
 
     @Override
     public <T> T get(String key, Class<T> tClass) {
-        log.debug("Get from Redis key is {} ", key);
+        logger.debug("Get from Redis key is {} ", key);
         Object val = redisTemplate.opsForValue().get(key);
         if (val == null) {
             return null;
@@ -66,28 +73,60 @@ public class RedisCacheServiceImpl implements RedisCacheService {
 
     @Override
     public void setAdd(String key, Object... value) {
+        logger.debug("Redis Set Add key: {} value: {}", key, JSON.toJSONString(value));
         redisTemplate.opsForSet().add(key, value);
     }
 
     @Override
-    public boolean hasKey(String key) {
-        return redisTemplate.hasKey(key);
-    }
-
-    @Override
-    public long increment(String key, long delta) {
-        return redisTemplate.opsForValue().increment(key, 1);
-    }
-
-    @Override
-    public void hSet(String key, Map<String, String> map) {
-        if (StringUtils.isNotEmpty(key) && null != map) {
-            redisTemplate.boundHashOps(key).putAll(map);
+    public <T> Set<T> sGetAll(String key, Class<T> tClass) {
+        logger.debug("Redis Set get key: {}", key);
+        Set<T> setT = redisTemplate.opsForSet().members(key);
+        if (isPrimaryClass(tClass)) {
+            return setT;
+        } else {
+            Set<T> setObj = new HashSet<>(setT.size());
+            setT.forEach(v -> {
+                setObj.add(JSON.parseObject(v.toString(), tClass));
+            });
+            return setObj;
         }
     }
 
     @Override
-    public void hset(String key, Map<String, Object> map) {
+    public Set<String> sGetAll(String key) {
+        return sGetAll(key, String.class);
+    }
+
+    @Override
+    public Boolean isMember(String key, Object member) {
+        logger.debug("Redis[Set is member] key:{} member:{}", key, JSON.toJSONString(member));
+        return redisTemplate.opsForSet().isMember(key, member);
+    }
+
+
+    @Override
+    public <V> List<V> sMembersList(String key, Class<V> tClass) {
+        Set<V> set = sGetAll(key, tClass);
+        return set.stream().collect(Collectors.toList());
+    }
+
+    @Override
+    public Long sRemove(String key, Object member) {
+        logger.debug("Redis Set delete key:{} member: {}", key, JSON.toJSONString(member));
+        return redisTemplate.opsForSet().remove(key, member);
+    }
+
+    @Override
+    public void hSet(String key, String hKey, Object hValue) {
+        logger.debug("hSet redis key: {} hKey: {} hValue: {}", key, hKey, JSON.toJSONString(hValue));
+        if (StringUtils.isNotEmpty(key) && StringUtils.isNotEmpty(hKey)) {
+            redisTemplate.boundHashOps(key).put(hKey, hValue);
+        }
+    }
+
+    @Override
+    public void hSet(String key, Map<String, Object> map) {
+        logger.debug("hSet redis key: {} value: {}", key, JSON.toJSONString(map));
         if (StringUtils.isNotEmpty(key) && null != map) {
             redisTemplate.boundHashOps(key).putAll(map);
         }
@@ -95,6 +134,7 @@ public class RedisCacheServiceImpl implements RedisCacheService {
 
     @Override
     public <K, V> Map<K, V> hGetAll(String key, Class<V> vClass) {
+        logger.debug("hGetAll redis key: {} ", key);
         Map<K, V> map = redisTemplate.opsForHash().entries(key);
         if (isPrimaryClass(vClass)) {
             return map;
@@ -109,6 +149,142 @@ public class RedisCacheServiceImpl implements RedisCacheService {
     @Override
     public Map<String, String> hGetAll(String key) {
         return hGetAll(key, String.class);
+    }
+
+    @Override
+    public <V> V hGet(String key, String hashKey, Class<V> tClass) {
+        logger.debug("hGet key: {} hashKey: {}", key, hashKey);
+        BoundHashOperations<String, String, Object> boundHashOperations = redisTemplate.boundHashOps(key);
+        Object value = boundHashOperations.get(hashKey);
+        if (isPrimaryClass(tClass)) {
+            return (V) value;
+        } else {
+            return JSON.parseObject(boundHashOperations.get(hashKey).toString(), tClass);
+        }
+    }
+
+    @Override
+    public String hGet(String key, String hashKey) {
+        return hGet(key, hashKey, String.class);
+    }
+
+    @Override
+    public <T> List<T> hGetAllList(String key, Class<T> clazz) {
+        Map<String, T> map = hGetAll(key, clazz);
+        if (null != map) {
+            return map.values().stream().collect(Collectors.toList());
+        }
+        return new ArrayList<>(0);
+    }
+
+
+
+    @Override
+    public <T> T leftPop(String key) {
+        Object val = redisTemplate.opsForList().leftPop(key);
+        if (val == null) {
+            return null;
+        }
+        return (T) val;
+    }
+
+    @Override
+    public Long leftPush(String key, Object value) {
+        logger.debug("leftPush key: {} value: {}", key, JSON.toJSONString(value));
+        return redisTemplate.opsForList().leftPush(key, value);
+    }
+
+    @Override
+    public Long leftPushAll(String key, Object... values) {
+        logger.debug("leftPushAll key: {} value: {}", key, JSON.toJSONString(values));
+        return redisTemplate.opsForList().leftPushAll(key, values);
+    }
+
+    @Override
+    public Long rightPush(String key, Object value) {
+        logger.debug("rightPush key: {} value: {}", key, JSON.toJSONString(value));
+        return redisTemplate.opsForList().rightPush(key, value);
+    }
+
+    @Override
+    public Long rightPushAll(String key, Object... values) {
+        logger.debug("rightPushAll key: {} value: {}", key, JSON.toJSONString(values));
+        return redisTemplate.opsForList().rightPushAll(key,values);
+    }
+
+    @Override
+    public <T> T rightPop(String key) {
+        logger.debug("RightPop from Redis key is {} ", key);
+        Object val = redisTemplate.opsForList().rightPop(key);
+        if (val == null) {
+            return null;
+        }
+        return (T) val;
+    }
+
+    @Override
+    public <T> List<T> lRange(String key, long start, long end, Class<T> tClass) {
+        logger.debug("lRange key: {} start: {} end: {}", key, start, end);
+        List<T> list = redisTemplate.opsForList().range(key, start, end);
+        if(isPrimaryClass(tClass)){
+            return list;
+        }else{
+            List<T> list1 = new ArrayList<>();
+            list.forEach(s -> list1.add(JSON.parseObject(s.toString(), tClass)));
+            return list1;
+        }
+    }
+
+    @Override
+    public long hDel(String key, String hashKey) {
+        logger.debug("hDel key: {} hashKey: {}", key, hashKey);
+        return redisTemplate.boundHashOps(key).delete(hashKey);
+    }
+
+    @Override
+    public long ttl(String key) {
+        return redisTemplate.getExpire(key);
+    }
+
+    @Override
+    public boolean hasHKey(String key, String hashKey) {
+        return redisTemplate.boundHashOps(key).hasKey(hashKey);
+    }
+
+    @Override
+    public boolean hasKey(String key) {
+        return false;
+    }
+
+    @Override
+    public Long increment(String key) {
+        logger.debug("increment key: {} ,delta: 1", key);
+        return redisTemplate.opsForValue().increment(key, 1L);
+    }
+
+    @Override
+    public Long increment(String key, long delta) {
+        logger.debug("increment key: {} ,delta: {}", key, delta);
+        return redisTemplate.opsForValue().increment(key, delta);
+    }
+
+    @Override
+    public Boolean expire(String key, long timeout, TimeUnit unit) {
+        logger.debug("expire key: {} timeout: {} unit: {}", key, timeout, unit);
+        return redisTemplate.expire(key, timeout, unit);
+    }
+
+
+    @Override
+    public Boolean expireAt(String key, long unixTime) {
+        logger.debug("expireAt key: {} atTime: {}", key, unixTime);
+        return redisTemplate.expireAt(key, new Date(unixTime));
+    }
+
+    @Override
+    public boolean setNx(String key, Object value) {
+        logger.debug("setNx redis key: {} value: {}", key, JSON.toJSONString(value));
+        return redisTemplate.opsForValue().setIfAbsent(key, value);
     }
 
     private boolean isPrimaryClass(Class vClass) {
