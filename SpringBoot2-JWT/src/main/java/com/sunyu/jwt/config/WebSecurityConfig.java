311078269d987d00b2a8c1dcea8b59beb6ba6030
oppofind <836575280@qq.com>
2019-04-21 21:13:32 +0800

    添加jwt认证

---
+++ SpringBoot2-JWT/src/main/java/com/sunyu/jwt/config/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.sunyu.jwt.config;
+
+import com.sunyu.jwt.filter.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * @author yu 2019/4/21.
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                // We filter the api/** requests
+                .addFilterBefore(new JwtAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
+    }
+
+
+}
