f7be94abd0efe95128089d8f5df386d50ed4544b
oppofind <836575280@qq.com>
2019-12-13 17:53:25 +0800

    更新文档

--- SpringBoot2-Kafka/src/main/java/com/sunyu/kafka/controller/KafkaController.java
+++ SpringBoot2-Kafka/src/main/java/com/sunyu/kafka/controller/KafkaController.java
@@ -14,6 +14,7 @@ import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.bind.annotation.*;
 
 /**
+ * kafka消息发送测试
  * @author yu 2018/11/9.
  */
 @Slf4j
